4b983442be27dd74caff2b0b0f71f85d3f8b4519
Salju <61861078+Jusey1@users.noreply.github.com>
2023-06-04 00:53:04 -0400

    Add files via upload

---
+++ src/main/java/net/salju/woodster/init/WoodsterConfig.java
@@ -0,0 +1,19 @@
+package net.salju.woodster.init;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class WoodsterConfig {
+	public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+	public static final ForgeConfigSpec CONFIG;
+
+	public static final ForgeConfigSpec.BooleanValue BOOKSHELF;
+	public static final ForgeConfigSpec.BooleanValue LADDER;
+	
+	static {
+		BUILDER.push("Registry");
+		BOOKSHELF = BUILDER.comment("Should Bookshelves be added?").define("Bookshelves", true);
+		LADDER = BUILDER.comment("Should Ladders be added?").define("Ladders", true);
+		BUILDER.pop();
+		CONFIG = BUILDER.build();
+	}
+}
--- src/main/java/net/salju/woodster/init/WoodsterItems.java
+++ src/main/java/net/salju/woodster/init/WoodsterItems.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.BlockItem;
 
 public class WoodsterItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, WoodsterMod.MODID);
+
 	public static final RegistryObject<Item> OAK_CHISELED_BOOKSHELF = block(WoodsterBlocks.OAK_CHISELED_BOOKSHELF, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> SPRUCE_CHISELED_BOOKSHELF = block(WoodsterBlocks.SPRUCE_CHISELED_BOOKSHELF, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> BIRCH_CHISELED_BOOKSHELF = block(WoodsterBlocks.BIRCH_CHISELED_BOOKSHELF, WoodsterTabs.WOODSTER);
@@ -22,6 +23,7 @@ public class WoodsterItems {
 	public static final RegistryObject<Item> MANGROVE_CHISELED_BOOKSHELF = block(WoodsterBlocks.MANGROVE_CHISELED_BOOKSHELF, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> CRIMSON_CHISELED_BOOKSHELF = block(WoodsterBlocks.CRIMSON_CHISELED_BOOKSHELF, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> WARPED_CHISELED_BOOKSHELF = block(WoodsterBlocks.WARPED_CHISELED_BOOKSHELF, WoodsterTabs.WOODSTER);
+
 	public static final RegistryObject<Item> SPRUCE_BOOKSHELF = block(WoodsterBlocks.SPRUCE_BOOKSHELF, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> BIRCH_BOOKSHELF = block(WoodsterBlocks.BIRCH_BOOKSHELF, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> JUNGLE_BOOKSHELF = block(WoodsterBlocks.JUNGLE_BOOKSHELF, WoodsterTabs.WOODSTER);
@@ -30,6 +32,7 @@ public class WoodsterItems {
 	public static final RegistryObject<Item> MANGROVE_BOOKSHELF = block(WoodsterBlocks.MANGROVE_BOOKSHELF, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> CRIMSON_BOOKSHELF = block(WoodsterBlocks.CRIMSON_BOOKSHELF, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> WARPED_BOOKSHELF = block(WoodsterBlocks.WARPED_BOOKSHELF, WoodsterTabs.WOODSTER);
+
 	public static final RegistryObject<Item> SPRUCE_LADDER = block(WoodsterBlocks.SPRUCE_LADDER, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> BIRCH_LADDER = block(WoodsterBlocks.BIRCH_LADDER, WoodsterTabs.WOODSTER);
 	public static final RegistryObject<Item> JUNGLE_LADDER = block(WoodsterBlocks.JUNGLE_LADDER, WoodsterTabs.WOODSTER);
