8349efa0d3fc16cc21d54929ba1989392ae8d26b
Salju <61861078+Jusey1@users.noreply.github.com>
2023-06-18 14:44:39 -0400

    Add files via upload

--- src/main/java/net/salju/woodster/init/WoodsterBlockEntities.java
+++ src/main/java/net/salju/woodster/init/WoodsterBlockEntities.java
@@ -1,6 +1,5 @@
 package net.salju.woodster.init;
 
-import net.salju.woodster.block.entity.*;
 import net.salju.woodster.block.*;
 import net.salju.woodster.WoodsterMod;
 
@@ -8,14 +7,15 @@ import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
 
+import net.minecraft.world.level.block.entity.ChiseledBookShelfBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.Block;
 
 public class WoodsterBlockEntities {
 	public static final DeferredRegister<BlockEntityType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, WoodsterMod.MODID);
-	public static final RegistryObject<BlockEntityType<?>> CHISELED_BOOKSHELF = REGISTRY.register("chiseled_bookshelf", () -> BlockEntityType.Builder.of(ChiseledBookshelfBlockEntity::new, (Block[]) findBlocks(ChiseledBookshelfBlock.class)).build(null));
+	public static final RegistryObject<BlockEntityType<?>> CHISELED_BOOKSHELF = REGISTRY.register("chiseled_bookshelf", () -> BlockEntityType.Builder.of(ChiseledBookShelfBlockEntity::new, (Block[]) findBlocks(ChiseledBookshelfBlock.class)).build(null));
 
 	public static Block[] findBlocks(Class<?> targets) {
 		return ForgeRegistries.BLOCKS.getValues().stream().filter(targets::isInstance).toArray(Block[]::new);
 	}
-}
+}
