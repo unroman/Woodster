3f312d24d0e96989b9f2482fbc564dec6484182c
Salju <61861078+Jusey1@users.noreply.github.com>
2023-06-18 14:44:19 -0400

    Delete WoodsterBlockProperties.java

--- src/main/java/net/salju/woodster/init/WoodsterBlockProperties.java
+++
@@ -1,7 +0,0 @@
-package net.salju.woodster.init;
-
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-
-public class WoodsterBlockProperties {
-	public static final IntegerProperty BOOKS = IntegerProperty.create("books", 0, 6);
-}
