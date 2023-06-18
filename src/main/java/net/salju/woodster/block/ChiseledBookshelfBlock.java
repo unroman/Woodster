045638a5339e8bfae6b5bc0938749faf74055639
Salju <61861078+Jusey1@users.noreply.github.com>
2023-06-18 14:45:18 -0400

    Add files via upload

--- src/main/java/net/salju/woodster/block/ChiseledBookshelfBlock.java
+++ src/main/java/net/salju/woodster/block/ChiseledBookshelfBlock.java
@@ -1,162 +1,44 @@
+package net.salju.woodster.block;
 
-package net.salju.woodster.block;
-
-import net.salju.woodster.init.WoodsterBlockProperties;
-import net.salju.woodster.block.entity.ChiseledBookshelfBlockEntity;
-
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.entity.ChiseledBookShelfBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.Containers;
-import net.minecraft.util.RandomSource;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.ChiseledBookShelfBlock;
 import net.minecraft.core.Direction;
-import net.minecraft.core.BlockPos;
 
-public class ChiseledBookshelfBlock extends BaseEntityBlock {
-	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
-	public static final IntegerProperty BOOKS = WoodsterBlockProperties.BOOKS;
-
+public class ChiseledBookshelfBlock extends ChiseledBookShelfBlock {
 	public ChiseledBookshelfBlock(BlockBehaviour.Properties props) {
 		super(props);
-		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(BOOKS, Integer.valueOf(0)));
-	}
-
-	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		super.createBlockStateDefinition(builder);
-		builder.add(FACING, BOOKS);
-	}
-
-	@Override
-	public RenderShape getRenderShape(BlockState state) {
-		return RenderShape.MODEL;
-	}
-
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
-	}
-
-	@Override
-	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-		super.use(state, world, pos, player, hand, hit);
-		if (hit.getDirection() == state.getValue(FACING)) {
-			BlockEntity target = world.getBlockEntity(pos);
-			if (target instanceof ChiseledBookshelfBlockEntity be) {
-				player.openMenu(be);
-				be.startOpen(player);
-			}
-			return InteractionResult.SUCCESS;
+		BlockState state = this.stateDefinition.any().setValue(HorizontalDirectionalBlock.FACING, Direction.NORTH);
+		for (BooleanProperty bool : ChiseledBookShelfBlock.SLOT_OCCUPIED_PROPERTIES) {
+			state = state.setValue(bool, Boolean.valueOf(false));
 		}
-		return InteractionResult.PASS;
-	}
-
-	@Override
-	public void onPlace(BlockState state, Level world, BlockPos pos, BlockState oldState, boolean moving) {
-		super.onPlace(state, world, pos, oldState, moving);
-		world.scheduleTick(pos, this, 5);
-	}
-
-	@Override
-	public void setPlacedBy(Level world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
-		if (stack.hasCustomHoverName()) {
-			BlockEntity target = world.getBlockEntity(pos);
-			if (target instanceof ChiseledBookshelfBlockEntity be) {
-				be.setCustomName(stack.getHoverName());
-			}
-		}
-	}
-
-	@Override
-	public void onRemove(BlockState state, Level world, BlockPos pos, BlockState newState, boolean isMoving) {
-		if (state.getBlock() != newState.getBlock()) {
-			BlockEntity target = world.getBlockEntity(pos);
-			if (target instanceof ChiseledBookshelfBlockEntity be) {
-				Containers.dropContents(world, pos, be);
-				world.updateNeighbourForOutputSignal(pos, this);
-			}
-			super.onRemove(state, world, pos, newState, isMoving);
-		}
-	}
-
-	@Override
-	public void tick(BlockState state, ServerLevel world, BlockPos pos, RandomSource random) {
-		BlockEntity target = world.getBlockEntity(pos);
-		if (target instanceof ChiseledBookshelfBlockEntity be) {
-			be.updateBlockState(state);
-		}
-		world.updateNeighbourForOutputSignal(pos, this);
-		world.scheduleTick(pos, this, 5);
-	}
-
-	@Override
-	public boolean hasAnalogOutputSignal(BlockState state) {
-		return true;
-	}
-
-	@Override
-	public int getAnalogOutputSignal(BlockState state, Level world, BlockPos pos) {
-		BlockEntity target = world.getBlockEntity(pos);
-		if (target instanceof ChiseledBookshelfBlockEntity be) {
-			int i = be.getLastSlot();
-			return Integer.valueOf(Math.min(15, i + 1));
-		}
-		return 0;
-	}
-
-	@Override
-	public boolean canConnectRedstone(BlockState state, BlockGetter world, BlockPos pos, Direction side) {
-		return state.getValue(FACING) == side;
-	}
-
-	@Override
-	public boolean isSignalSource(BlockState state) {
-		return true;
-	}
-
-	@Override
-	public int getSignal(BlockState state, BlockGetter world, BlockPos pos, Direction side) {
-		BlockEntity target = world.getBlockEntity(pos);
-		int i = (this.getBooks(target));
-		return state.getValue(FACING) == side ? Integer.valueOf(Math.min(15, i * 2)) : 0;
-	}
-
-	@Override
-	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-		return new ChiseledBookshelfBlockEntity(pos, state);
-	}
-
-	@Override
-	public float getEnchantPowerBonus(BlockState state, LevelReader world, BlockPos pos) {
-		BlockEntity target = world.getBlockEntity(pos);
-		return (this.getBooks(target) >= 3) ? 1f : 0f;
+		this.registerDefaultState(state);
 	}
 
 	public static int getBooks(BlockEntity target) {
 		int books = 0;
-		if (target instanceof ChiseledBookshelfBlockEntity be) {
-			for (ItemStack stack : be.stacks) {
-				if (!stack.isEmpty()) {
-					books = (books + 1);
-				}
+		if (target instanceof ChiseledBookShelfBlockEntity be) {
+			if (!(be.getItem(0).isEmpty())) {
+				books = (books + 1);
+			}
+			if (!(be.getItem(1).isEmpty())) {
+				books = (books + 1);
+			}
+			if (!(be.getItem(2).isEmpty())) {
+				books = (books + 1);
+			}
+			if (!(be.getItem(3).isEmpty())) {
+				books = (books + 1);
+			}
+			if (!(be.getItem(4).isEmpty())) {
+				books = (books + 1);
+			}
+			if (!(be.getItem(5).isEmpty())) {
+				books = (books + 1);
 			}
 		}
 		return books;
